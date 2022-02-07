1761daf99ab618f522e7401ff9370682ae36dd12
joel4959 <joelm920424@gmail.com>
2022-02-07 08:38:08 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *main - main
+ *Return: 0
+ *
+ *description: functions_nested_loops
+ */
+int main(void)
+{
+	char p[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 7; i++)
+	{
+		_putchar(p[i]);
+	}
+
+	_putchar('\n');
+	return (0);
+}
