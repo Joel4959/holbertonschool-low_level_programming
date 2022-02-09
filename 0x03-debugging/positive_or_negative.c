4f940fb9ed7a42f20a72afb5c6b68c096e1a9b1a
joel4959 <joelm920424@gmail.com>
2022-02-09 06:04:45 -0800

    positive_or_negative.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+* positive_or_negative - comprobation
+* @n: Multiple mains
+* Return: n
+*/
+
+int positive_or_negative(int n)
+{
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+	return (n);
+}
