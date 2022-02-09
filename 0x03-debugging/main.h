4697e8e02bba8a58feb47970d2c7063e0c83a3bb
joel4959 <joelm920424@gmail.com>
2022-02-09 11:26:43 -0800

    positive_or_negative

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stddef.h>
 
-void positive_or_negative(int n);
+int positive_or_negative(int n);
 
 #endif /* MAIN_H */
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,26 +1,26 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "main.h"
 #include <time.h>
+
 /**
-* positive_or_negative - debugging
-*@n: Multiple mains
-* Return: 0
-*
-*/
+  * positive_or_negative - debugging
+  * @n: Multiple mains
+  * Return: 0
+  *
+  */
 int positive_or_negative(int n)
 {
 
 	if (n > 0)
-	{
+{
 		printf("%d is positive\n", n);
-	}
+}
 	else if (n < 0)
-	{
+{
 		printf("%d is negative\n", n);
-	}
+}
 	else
-	{
+{
 		printf("%d is zero\n", n);
-	}
+}
 	return (0);
 }
